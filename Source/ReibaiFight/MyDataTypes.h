d7564da7d5619864a9f34bd979fa03360632817e
1210555 <syu1210.3595@gmail.com>
2025-11-07 03:18:38 +0900

    メタタグの追加、回復等の再抽選可能なものを再抽選可能にするbool型bIsRepeatable変数追加

--- Source/ReibaiFight/MyDataTypes.h
+++ Source/ReibaiFight/MyDataTypes.h
@@ -29,50 +29,50 @@ public:
     //UIに表示用のデータテーブルの各要素
 
     //強化先のタイトル、ちょっとでっかく
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "メインの強化名"))
     FText UpgradeName;
 
     //強化先の概要、説明。タイトルより小さく
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "強化の内容、説明"))
     FText UpgradeDescription;
 
     //強化先のアイコン
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "強化のアイコン画像"))
     TObjectPtr<UTexture2D> UpgradeIcon;
 
     //行の名前。これで自動斬撃3,4のような強化版を入手するロジックに必要
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "事前に有していなければならない '行' の名前。レベル1,2…のように上がっていくものは必要"))
     FName PrerequisiteID;
-
-public:
-    //強化の定義をここに追加していく
 
     // このアップグレードの種類
 	//FUpgradeDataの上で定義しているenum classがEUpgradeType
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "新しい攻撃の追加、体力回復・身体強化、既存の攻撃の攻撃の強化"))
     EUpgradeType Type;
 
     // (AddNewAttack用) 追加する攻撃コンポーネントのブループリント
 	//UAttackComponentBaseを親に持つクラスのブループリントを格納できる変数
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "ここで設定するBPをもとにアタックコンポーネント生成"))
     TSubclassOf<UAttackComponentBase> AttackComponentClass;
 
     // (ModifyPlayerStat用) 変更するステータスの名前
 	//体力回復などはここに"MaxHealth"が入る
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "ModifyPlayerStatの値を変化する場合何の値を変更するかを記述"))
     FName PlayerStatToModify;
 
     // (ModifyAttackStat用) 強化する攻撃コンポーネントのタグ
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "BPで設定しているタグと一致しているものをレベルアップ時に探してくる"))
     FName AttackComponentTag;
 
     // 強化する量
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "攻撃の威力をどれだけ増やすか(float型)"))
     float ModificationAttackAmount;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Tooltip = "攻撃の頻度をどれだけ短縮するか(記述した % だけ短縮)"))
     float ModificationFrequentValue;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, meta=(Tooltip = "体力の回復などModifyPlayerStatの値をどれだけ変更するか"))
     float ModificationValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,meta = (Tooltip = "何度でも取得可能なアップグレードはtrueにする"))
+	bool bIsRepeatable;
 };
